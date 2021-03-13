30f73f3e13f7d412b46463f9880dec4216314d40
CitricVenuss <a01379766@itesm.mx>
2021-03-13 17:51:03 -0600

    El personaje se mueve, dispara, cuando labala hace trigger la bala da una animacion, falta ver el que al disparar la bala, no le afecte al collaider del personaje ya que sale de enmedio del personaje y al detectar la collision lo mueve o se queda detras de el

---
+++ Assets/Bullet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+  
+    public GameObject explosion;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+      ;
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player")){
+       
+        }
+        if(collision.CompareTag("Piedra") || collision.CompareTag("Claca"))
+        {
+        GameObject effect = Instantiate(explosion,transform.position,Quaternion.identity);
+        Destroy(effect,.5f);
+        Destroy(gameObject);
+        }
+    }
+    void OnCollision2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            
+        }
+        else
+        {
+        GameObject effect = Instantiate(explosion,transform.position,Quaternion.identity);
+        Destroy(effect,.5f);
+        Destroy(gameObject);
+        }
+    }
+ 
+
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player : MonoBehaviour
 {
     public float speed  = 5f;
+    public float speed_bullet = 20f;
     public Rigidbody2D rb;
     Vector2 movement;
     public Animator animator;
-    // Start is called before the first frame update
+ 
+
+    public GameObject crosshair;
+    public float crosshairdist = 0.1f;
+    public GameObject bulletPrefab;
+
+
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
@@ -23,10 +29,38 @@ public class Player : MonoBehaviour
         animator.SetFloat("Horizontal",movement.x);
         animator.SetFloat("Vertical",movement.y);
         animator.SetFloat("Speed",movement.sqrMagnitude);
-        
+        Aim();
+        Shoot();
+      
     }
+ 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+       
     }
+
+    void Aim()
+    {   
+        if(movement != Vector2.zero){
+             crosshair.transform.localPosition = movement * crosshairdist;
+        }
+    }
+    void Shoot()
+    {
+        Vector2 shootdir = crosshair.transform.localPosition;
+        shootdir.Normalize();
+        if(Input.GetButtonUp("Fire1")){
+
+            GameObject bullet = Instantiate(bulletPrefab,(transform.position), Quaternion.identity);
+            bullet.GetComponent<Rigidbody2D>().velocity = shootdir * speed_bullet;
+            bullet.transform.Rotate(0,0,Mathf.Atan2(shootdir.y,shootdir.x) * Mathf.Rad2Deg);
+            Destroy(bullet,2f);
+
+        }
+        
+    }
+    
+
+    
 }
